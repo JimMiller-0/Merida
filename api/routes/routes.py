b05965cfb5e1fba559341826c2c2439aaa5b356f
Jim Miller <james.miller319@gmail.com>
2023-09-03 13:12:49 -0400

    adding structure

---
+++ api/routes/routes.py
@@ -0,0 +1,19 @@
+from services.input.promptinjection import api as input_pi_checks_ns
+from services.input.safety import api as input_safety_ns
+from services.output.out_safety import api as output_safety_ns
+from services.threatmgmt import api as threatmgmt_ns
+
+from flask import Blueprint
+from flask_restx import Api
+
+blueprint = Blueprint("api", __name__)
+
+api = Api(
+    blueprint, title="Merida API", version="1.0", description="LLM Application Security Service API"
+)
+
+
+api.add_namespace(input_pi_checks_ns, path="/input/pi")
+api.add_namespace(input_safety_ns, path="/input/safety")
+api.add_namespace(output_safety_ns, path="/output/safety")
+api.add_namespace(threatmgmt_ns, path="/threatmgmt")
---
+++ api/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    client = app.test_client()
+    return client
+
+
+
