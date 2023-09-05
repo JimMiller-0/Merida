44ed96ded6777caa65eb4b77485a8c20ea9b09de
Jim Miller <james.miller319@gmail.com>
2023-09-05 16:19:20 -0400

    PII done

--- api/services/input/safety.py
+++ api/services/input/safety.py
@@ -4,51 +4,28 @@ import os
 import google.cloud.dlp
 from typing import List
 
-api = Namespace("dogs", description="Dogs related operations")
+api = Namespace("dlp", description="inspect user input for PII and redact")
 
-dog = api.model(
-    "Dog",
+dlp = api.model(
+    "dlp",
     {
-        "id": fields.String(required=True, description="The dog identifier"),
-        "name": fields.String(required=True, description="The dog name"),
-    },
+        "user_input": fields.String(description="original text"),
+        "dlp_output": fields.String(description="redacted text"), 
+    }
 )
 
-DOGS = [
-    {"id": "medor", "name": "Medor"},
+DLP_RESPONSE = [
+
+    {"user_input": "medor", "dlp_output": "Medor"},
 ]
 
-
-@api.route("/")
-class DogList(Resource):
-    @api.doc("list_dogs")
-    @api.marshal_list_with(dog)
-    def get(self):
-        """List all dogs"""
-        return DOGS
-
-
-@api.route("/<id>")
-@api.param("id", "The dog identifier")
-@api.response(404, "Dog not found")
-class Dog(Resource):
-    @api.doc("get_dog")
-    @api.marshal_with(dog)
-    def get(self, id):
-        """Fetch a dog given its identifier"""
-        for dog in DOGS:
-            if dog["id"] == id:
-                return dog
-        api.abort(404)
-
-
-@api.route("/DLP", methods=["POST"])
+@api.route("/dlp", methods=["POST"])
 @api.response(404, "Error")
 class DLP(Resource):
     @api.doc("post_dlp")
-    def dlp():
-        if request.method == "POST":
-            user_text = request.form("user_input")
+   # @api.marshal_with(dlp)
+    def post(self):
+            user_text = request.form.get("user_input")
             PROJECT_ID=os.environ.get("PROJECT_ID")
 
             def deidentify_with_replace_infotype(
@@ -96,7 +73,7 @@ class DLP(Resource):
                 # Print out the results.
                 return response.item.value
 
-            deidentify_with_replace_infotype(PROJECT_ID, user_text.text, ["PERSON_NAME","EMAIL_ADDRESS"])
+            dlp_output=deidentify_with_replace_infotype(PROJECT_ID, user_text, ["PERSON_NAME","EMAIL_ADDRESS"])
 
-        else:
-            api.abort(404)
+            return jsonify(dlp_output)
+
