b24f598cbeafaa01c52e94519ef6d15fe2ace33c
Jim Miller <james.miller319@gmail.com>
2023-09-07 10:34:45 -0400

    db connection

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ LLM Application Security Service (LASS)
 1. pip install -r requirements.txt
 2. export PROJECT_ID=<project_id>
 3. export LOCATION=us-central1
+3. export INSTANCE_HOST=127.0.0.1
+5. export DB_USER=postgres
+6. export DB_PASS=<db password>
+7. export DB_NAME=merida-db
+8. export DB_PORT=5432
+4. gcloud alloydb instances list
+5. export INSTANCE_URI=<instance uri from last command>
+6. nohup ./alloydb-auth-proxy $INSTANCE_URI
+
 4. flask run
--- api/app.py
+++ api/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import make_response
 from api.routes.routes import blueprint as api
+import sqlalchemy
+import logging
+import os
+
+from api.config.connect_tcp import connect_tcp_socket
 
 
 # flask app factory to create app
@@ -8,6 +13,18 @@ from api.routes.routes import blueprint as api
 
 def create_app():
     app = Flask(__name__)
+
+    logger = logging.getLogger()
+
+    def init_connection_pool() -> sqlalchemy.engine.base.Engine:
+    # use a TCP socket when INSTANCE_HOST (e.g. 127.0.0.1) is defined
+        if os.environ.get("INSTANCE_HOST"):
+            return connect_tcp_socket()
+
+        raise ValueError(
+            "Missing database connection parameter. Please define INSTANCE_HOST"
+        )
+
 
     @app.route("/", methods=["GET"])
     def home():
