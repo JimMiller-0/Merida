cb5caa11417fd2bab2e59df8628ebf1d96d70dc9
Jim Miller <james.miller319@gmail.com>
2023-09-12 14:22:40 -0400

    threatmgmt

--- api/services/threatmgmt.py
+++ api/services/threatmgmt.py
@@ -1,38 +1,39 @@
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource, fields 
+from flask import Flask, jsonify, request, Response, make_response 
+import os 
+import sys
+import asyncio
+import vertexai 
+from datetime import datetime
+import sqlalchemy
+import logging   
+import google.cloud.dlp
+from typing import List, Dict
+from api.db.db import init_connection_pool
+from vertexai.preview.language_models import TextGenerationModel
+from langchain.llms.base import BaseLLM
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import VertexAI
 
-api = Namespace("dogs", description="Dogs related operations")
+api = Namespace("threat_mgmt", description="managing threats seen in production")
 
-dog = api.model(
-    "Dog",
+threatmgmt = api.model(
+    "Threat Management",
     {
         "id": fields.String(required=True, description="The dog identifier"),
         "name": fields.String(required=True, description="The dog name"),
     },
 )
 
-DOGS = [
+THREATMANAGEMENT = [
     {"id": "medor", "name": "Medor"},
 ]
 
 
 @api.route("/")
-class DogList(Resource):
-    @api.doc("list_dogs")
-    @api.marshal_list_with(dog)
+class ThreatList(Resource):
+    @api.doc("list_Threats")
+    #@api.marshal_list_with(threatmgmt)
     def get(self):
         """List all dogs"""
-        return DOGS
-
-
-@api.route("/<id>")
-@api.param("id", "The dog identifier")
-@api.response(404, "Dog not found")
-class Dog(Resource):
-    @api.doc("get_dog")
-    @api.marshal_with(dog)
-    def get(self, id):
-        """Fetch a dog given its identifier"""
-        for dog in DOGS:
-            if dog["id"] == id:
-                return dog
-        api.abort(404)
+        return make_response({"pi_detect":"This API is for detecting prompt injection in user input"}, 200)
